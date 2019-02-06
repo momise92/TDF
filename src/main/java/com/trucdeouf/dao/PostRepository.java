ecd8b1e295bb1feff0c9c1d3c9386777f28e69f1
momise92 <boss973@hotmail.fr>
2019-02-06 17:51:41 +0100

    update repository pagesorting by jpa

--- src/main/java/com/trucdeouf/dao/PostRepository.java
+++ src/main/java/com/trucdeouf/dao/PostRepository.java
@@ -3,13 +3,13 @@ package com.trucdeouf.dao;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.trucdeouf.entities.Post;
 
 @RepositoryRestResource
-public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 	List<Post> findAllByOrderByCreateDateDesc();
 
